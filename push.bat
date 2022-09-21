aa77bbd2f29ce0733468b497a84ec43671ab220c
CIT142G-35@M306.uc-bcf.edu.ph <CIT142G-35@M306.uc-bcf.edu.ph>
2022-09-21 15:40:31 +0800

    Auto upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off 
-git remote set-url origin https://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@github.com/Marshhh2022/Marj.git
+git remote set-url origin https://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@github.com/Marshhh2022/Marj.git
 pause
 git add .
 git commit -m "Auto upload"
